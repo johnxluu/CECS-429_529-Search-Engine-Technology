931ad8d40cd3d6d6c059a9143709c328d47f90ce
johnxluu <littleluu98@yahoo.com>
2022-06-01 17:23:46 -0700

    Documents-and-Indexes

---
+++ bin/cecs429/documents/Document.java
@@ -0,0 +1,26 @@
+package cecs429.documents;
+
+import java.io.Reader;
+import java.nio.file.Path;
+
+/**
+ * Represents a document in an index.
+ */
+public interface Document {
+	/**
+	 * The ID used by the index to represent the document.
+	 */
+	int getId();
+	
+	/**
+	 * Gets a stream over the content of the document.
+	 */
+	Reader getContent();
+	
+	/**
+	 * The title of the document, for displaying to the user.
+	 */
+	String getTitle();
+	
+	Path getFilePath();
+}
---
+++ bin/cecs429/indexes/Posting.java
@@ -0,0 +1,23 @@
+package cecs429.indexes;
+
+import java.util.List;
+
+/**
+ * A Posting encapulates a document ID associated with a search query component.
+ */
+public class Posting {
+	private int mDocumentId;
+	private List<Integer> positions;
+	
+	public Posting(int documentId) {
+		mDocumentId = documentId;
+	}
+	
+	public int getDocumentId() {
+		return mDocumentId;
+	}
+	
+	public List<Integer> getPositions(int documentId){
+		return positions;
+	}
+}
