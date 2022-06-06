bc0488a954de260e66bb7d134dc84117256f8783
Venkatesh Ramoju <rnbvenkatesh@hotmail.com>
2022-06-05 19:26:09 -0700

    Support for Special Query

--- src/edu/csulb/TermDocumentIndexer.java
+++ src/edu/csulb/TermDocumentIndexer.java
@@ -2,22 +2,18 @@ package edu.csulb;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 import cecs429.documents.DirectoryCorpus;
 import cecs429.documents.Document;
 import cecs429.documents.DocumentCorpus;
 import cecs429.documents.JsonFileDocument;
 import cecs429.indexes.Index;
-import cecs429.indexes.InvertedIndex;
-import cecs429.indexes.PositionalIndexPosting;
 import cecs429.indexes.PositionalInvertedIndex;
 import cecs429.indexes.Posting;
 import cecs429.queries.BooleanQueryParser;
-import cecs429.text.BasicTokenProcessor;
 import cecs429.text.CustomTokenProcessor;
 import cecs429.text.EnglishTokenStream;
 
@@ -27,9 +23,15 @@ public class TermDocumentIndexer {
 		System.out.println("Enter Corpus Directory");
 		String corpusDirectory = sc.nextLine();
 //		corpusDirectory="F:\\CECS429_529\\corpus";
-		String fileType = getFileExtension(corpusDirectory);
+
 		// Create a DocumentCorpus to load documents from the project directory.
 		// F:\CECS429_529\corpus
+		processIndexingAndSearch(corpusDirectory);
+		sc.close();
+	}
+
+	private static void processIndexingAndSearch(String corpusDirectory) {
+		String fileType = getFileExtension(corpusDirectory);
 		DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(Paths.get(corpusDirectory).toAbsolutePath(),
 				fileType);
 		// Index the documents of the corpus.
@@ -39,7 +41,6 @@ public class TermDocumentIndexer {
 		long totalTime = endTime - startTime;
 		System.out.println("Time taken to index: " + totalTime + " milliseconds");
 		searchQuery(corpus, index);
-		sc.close();
 	}
 
 	private static void searchQuery(DocumentCorpus corpus, Index index) {
@@ -51,21 +52,52 @@ public class TermDocumentIndexer {
 			// single-term queries.
 			System.out.println("Enter query to search");
 			String query = scanner.nextLine();
-			System.out.println("Query: (" + query + ") found in the following documents");
-//			for (String eachQuery : customTokenProcessor.processToken(query)) {
-			BooleanQueryParser booleanQueryParser = new BooleanQueryParser();
-			List<Posting> postings=booleanQueryParser.parseQuery(query).getPostings(index);
+			if (isSpecialQuery(query)) {
+				processSpecialQuery(query, index);
+			} else {
+				System.out.println("Query: (" + query + ") found in the following documents");
+//				for (String eachQuery : customTokenProcessor.processToken(query)) {
+				BooleanQueryParser booleanQueryParser = new BooleanQueryParser();
+				List<Posting> postings = booleanQueryParser.parseQuery(query).getPostings(index);
 				for (Posting p : postings) {
 					System.out.println("Document " + corpus.getDocument(p.getDocumentId()).getTitle());
 				}
-//			}
-			System.out.println(postings.size());
+//				}
+				System.out.println(postings.size());
+			}
 			System.out.println("Want to search a new query? (Y/N)");
 			String newQuery = scanner.nextLine();
 			if (newQuery.equalsIgnoreCase("N")) {
 				shouldEnd = false;
 			}
 		}
+	}
+
+	private static void processSpecialQuery(String query, Index index) {
+		String[] splQuery = query.split(" ");
+		if (splQuery.length == 1 && splQuery[0].equalsIgnoreCase(":q")) {
+				System.exit(0);
+		} else if (splQuery.length == 1 && splQuery[0].equalsIgnoreCase(":vocab")) {
+				List<String> vocabList = index.getVocabulary();
+				int size = 1000;
+				if (vocabList.size() < 1000)
+					size = vocabList.size();
+				IntStream.range(0, size).mapToObj(i -> vocabList.get(i)).forEach(System.out::println);
+		} else if (splQuery.length == 2 && splQuery[0].equalsIgnoreCase(":stem")) {
+				CustomTokenProcessor tk = new CustomTokenProcessor();
+				System.out.println("Stemmed term: "+tk.getStem(splQuery[1]));
+		} else if(splQuery.length == 2 && splQuery[0].equalsIgnoreCase(":index")) {
+				processIndexingAndSearch(splQuery[1]);
+		} else {
+			System.out.println("Invalid Special Query");
+		}
+	}
+
+	private static boolean isSpecialQuery(String query) {
+		if (query.startsWith(":")) {
+			return true;
+		}
+		return false;
 	}
 
 	public static String getFileExtension(String path) {
@@ -82,14 +114,11 @@ public class TermDocumentIndexer {
 	}
 
 	private static Index indexCorpus(DocumentCorpus corpus, String fileExtension) {
-		HashSet<String> vocabulary = new HashSet<>();
-		BasicTokenProcessor processor = new BasicTokenProcessor();
 		CustomTokenProcessor customTokenProcessor = new CustomTokenProcessor();
 //		InvertedIndex index = new InvertedIndex();
 		PositionalInvertedIndex index = new PositionalInvertedIndex();
 		EnglishTokenStream es;
 		System.out.println("Index Processing started for " + corpus.getCorpusSize() + " Documents");
-		int c=0;
 
 		for (Document d : corpus.getDocuments()) {
 			int position = 0;
@@ -100,10 +129,9 @@ public class TermDocumentIndexer {
 				es = new EnglishTokenStream(d.getContent());
 			}
 			for (String token : es.getTokens()) {
-				
-				
+
 				List<String> term = customTokenProcessor.processToken(token);
-			
+
 				if (term.size() <= 1)
 					index.addTerm(term.get(0), d.getId(), position);
 				else {
